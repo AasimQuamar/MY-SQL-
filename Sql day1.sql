d64f73630b25f57782f35f54f21af60afe41b483
Aasim Quamar <146803881+AasimQuamar@users.noreply.github.com>
2025-09-09 17:29:04 +0530

    Add files via upload

---
+++ Sql day1.sql
@@ -0,0 +1,11 @@
+create database cybrom;  # TO CREATE THE OUTER STRUCTURE 
+use cybrom;  # TO USE THE DATABASES
+show engines;
+show tables;  # TO REPRESENT THE TABLE WITHIN THE DATABASES
+create database cybrom1;
+use cybrom1;
+use cm;
+show tables;
+select * from customers;
+select contactfirstname, contactlastname from customers;
+drop database cybrom;
